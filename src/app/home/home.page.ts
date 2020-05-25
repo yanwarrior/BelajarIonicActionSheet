571d5a64b4f4cd2689ced1a0b166bbdd97a261ee
yanwarsolah <yanwarsolah@gmail.com>
2020-05-26 05:22:24 +0700

    Menggunakan ActionSheet di Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  actionSheet: any;
 
-  constructor() {}
+  constructor(
+    private actionSheetController: ActionSheetController
+  ) {}
 
+  ngOnInit() {
+      this.actionSheet = this.actionSheetController.create({
+        header: 'Aksi',
+        buttons: [{
+          text: 'Hapus',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            console.log('Hapus diklik');
+          }
+        }, {
+          text: 'Bagikan',
+          icon: 'share',
+          handler: () => {
+            console.log('Bagikan diklik');
+          }
+        }, {
+          text: 'Mainkan (buka popup)',
+          icon: 'play',
+          handler: () => {
+            console.log('Mainkan diklik');
+          }
+        }, {
+          text: 'Suka',
+          icon: 'heart',
+          handler: () => {
+            console.log('Suka dikil');
+          }
+        }, {
+          text: 'Batal',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Batal diklik');
+          }
+        }]
+      })
+        .then(actionsheet => {
+          actionsheet.present();
+        });
+  }
 }
